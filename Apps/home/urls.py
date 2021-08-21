8ff43a1cf7efb347e898077842bdb1b1185e2320
Nery Zacarias <nerydota@gmail.com>
2021-08-21 17:28:31 -0600

    actividad2.2

--- Apps/home/urls.py
+++ Apps/home/urls.py
@@ -14,9 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path
+from .views import HomeView,CreditosView
+
+app_name='app1'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('home/', include('Apps.home.urls'))
+    path('index/', HomeView.as_view(), name='home'),
+    path('creditos/', CreditosView.as_view(), name='creditos'),
 ]
